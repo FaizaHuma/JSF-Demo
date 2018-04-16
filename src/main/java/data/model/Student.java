1910d82077864fa866aa21b1d7f6c350b6f6cb19
Arham Khan <arhamk47@gmail.com>
2018-04-16 13:28:11 +0600

    JSF Commit

---
+++ src/main/java/data/model/Student.java
@@ -0,0 +1,27 @@
+package data.model;
+
+public class Student {
+
+    private int studentId;
+    private String nameStudent;
+
+    public Student() {
+
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getNameStudent() {
+        return nameStudent;
+    }
+
+    public void setNameStudent(String nameStudent) {
+        this.nameStudent = nameStudent;
+    }
+}
